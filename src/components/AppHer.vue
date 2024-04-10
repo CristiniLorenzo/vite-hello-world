f3bae231790db22ab8598c57894d5c00f90de3a6
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-04-10 16:53:18 +0200

    first commit

---
+++ src/components/AppHer.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        name: 'AppHer',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+    
+<template>
+    <div>
+        <img src="../assets/hero.avif" alt="">
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            sayHello() {
+                alert('Hello Vite!!!')
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+    <button @click="sayHello">Saluta vite</button>
+</template>
